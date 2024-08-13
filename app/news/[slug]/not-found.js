a114a98753234da313d1cc7f756b841f41669f75
Surendra Yalakala <surendrayalakala@gmail.com>
2024-08-13 16:57:13 +0530

    not found error handled

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Not found !</h1>
+      <p>Unfortunately, we couldn't find the requested article!</p>
+    </div>
+  );
+}
