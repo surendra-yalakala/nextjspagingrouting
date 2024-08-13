175f6e9f27380b4034ff05d1162eb10d82754498
Surendra Yalakala <surendrayalakala@gmail.com>
2024-08-13 18:16:28 +0530

    catch all implemented

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,60 @@
+import NewsList from "@/components/news-list";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
+import Link from "next/link";
+
+export default function FilteredNewsPage({ params }) {
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No views found</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  const newsList = getNewsForYear(filter);
+
+  // return <NewsList news={newsList} />;
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}
--- app/archive/@archive/[year]/page.js
+++
@@ -1,9 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FilteredNewsPage({ params }) {
-  const newYear = params.year;
-  const newsList = getNewsForYear(newYear);
-
-  return <NewsList news={newsList} />;
-}
