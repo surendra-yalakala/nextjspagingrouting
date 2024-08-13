ba218979a2f554167be833b864f66ead963ccd0d
Surendra Yalakala <surendrayalakala@gmail.com>
2024-08-13 16:34:39 +0530

    main header and dummy data added

--- app/news/[id]/page.js
+++
@@ -1,9 +0,0 @@
-export default function NewsDetailsPage({ params }) {
-  const newsId = params.id;
-  return (
-    <>
-      <h1>News Details Page</h1>
-      <p>News Id: {newsId}</p>
-    </>
-  );
-}
--- app/news/page.js
+++ app/news/page.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
+import { DUMMY_NEWS } from "@/dummy-news";
+
 export default function NewsPage() {
   return (
     <>
       <h1>News page</h1>
-      <ul>
-        <li>
+      <ul className="news-list">
+        {/* <li>
           <Link href="/news/first-news">First News Item</Link>
         </li>
         <li>
@@ -13,7 +15,18 @@ export default function NewsPage() {
         </li>
         <li>
           <Link href="/news/third-news">Third News Item</Link>
-        </li>
+        </li> */}
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
