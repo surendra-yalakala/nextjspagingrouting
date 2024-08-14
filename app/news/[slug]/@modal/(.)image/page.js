9a04dd53797b02e8a97a9ada309820ab99fa45d4
Surendra Yalakala <surendrayalakala@gmail.com>
2024-08-14 10:46:57 +0530

    back nav added

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,7 +1,9 @@
+"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
   const newItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newItemSlug);
 
@@ -10,7 +12,7 @@ export default function InterceptedImagePage({ params }) {
   }
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
