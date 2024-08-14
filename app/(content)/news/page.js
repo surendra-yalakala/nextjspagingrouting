893c372270ebc2ee0e7a83d0e1e81b0a8d3e4f9f
Surendra Yalakala <surendrayalakala@gmail.com>
2024-08-14 11:55:01 +0530

    client side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,24 +1,37 @@
-import Link from "next/link";
+"use client";
 
-import { DUMMY_NEWS } from "@/dummy-news";
+// import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch(" http://localhost:3000/news");
+      if (!response.ok) {
+        setError("Failed to fetch news.");
+      }
+
+      const news = await response.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <>
       <h1>News page</h1>
-      <ul className="news-list">
-        {/* <li>
-          <Link href="/news/first-news">First News Item</Link>
-        </li>
-        <li>
-          <Link href="/news/second-news">Second News Item</Link>
-        </li>
-        <li>
-          <Link href="/news/third-news">Third News Item</Link>
-        </li> */}
-        <NewsList news={DUMMY_NEWS} />
-      </ul>
+      <ul className="news-list">{news && <NewsList news={news} />}</ul>
     </>
   );
 }
